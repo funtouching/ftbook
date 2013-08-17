9e1dbbd2e270e235d8212453e0bb2d8fa021c85e
Daniel F <danfarabi@gmail.com>
2013-08-18 00:32:17 +1000

    what a muppet

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ftbook::Application.config.secret_token = '2c6ee90029b38f2163c0ecbfd69bf0001aac37de6d6199120016f592c3e8087a511aad4b49232b8b23c0ec979d63431fab5dce6f73023ae98aac36990eff84ab'
